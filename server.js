cae27b08d7711aa915961aee813499042262d127
sphhax <zskaistis2@gmail.com>
2022-08-28 20:40:44 -0500

    fixed router

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
+
+app.route('/*')
+  .get(function(req, res) {
+        res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+module.exports = app;
